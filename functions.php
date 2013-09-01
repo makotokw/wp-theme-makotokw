94638fb4994ca8a9f140479c7663bb08b86111d0
makoto_kw <makoto.kw@gmail.com>
2013-09-01 21:27:25 +0900

    Use wp_shortlink_wp_head for Tospy

--- functions.php
+++ functions.php
@@ -69,7 +69,9 @@ if (!function_exists('makotokw_setup')) :
 		remove_action('wp_head', 'rsd_link');
 		remove_action('wp_head', 'wlwmanifest_link');
 		remove_action('wp_head', 'wp_generator');
-		remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+		
+		// Tospy may use shortlink
+		//remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
 
 		/*
 		 * Disable Jetpack OGP
