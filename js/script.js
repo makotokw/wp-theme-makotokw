16dd58a412fa2c839ce9ebb6b63c5a97e009e976
makoto_kw <makoto.kw@gmail.com>
2013-10-10 01:38:41 +0900

    Supported jQuery 1.8 or later

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 (function($){
-	if ($.browser.msie) {
-		if(parseInt($.browser.version) == 8) {
-			$("html").addClass("ie ie8");
-		} else if(parseInt($.browser.version) == 7) {
+	var ua = navigator.userAgent,
+		isIE = ua.match(/msie/i),
+		isIE7 = isIE && ua.match(/msie 7\./i),
+		isIE8 = isIE && ua.match(/msie 8\./i);
+	if (isIE) {
+		if (isIE7) {
 			$("html").addClass("ie ie7");
+		} else if (isIE8) {
+			$("html").addClass("ie ie8");
 		} else {
 			$("html").addClass("ie");
 		}
