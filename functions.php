5325906bec74902e2216898d8a1b42e94815e645
makoto_kw <makoto.kw@gmail.com>
2014-03-15 14:04:47 +0900

    Disable XML-RPC

--- functions.php
+++ functions.php
@@ -45,6 +45,11 @@ function makotokw_setup()
 	load_theme_textdomain('makotokw', get_template_directory() . '/languages');
 
 	/**
+	 * Disable XML RPC
+	 */
+	add_filter('xmlrpc_enabled', '__return_false');
+
+	/**
 	 * Add default posts and comments RSS feed links to head
 	 */
 	add_theme_support('automatic-feed-links');
