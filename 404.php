14a88a154b6268b29d78b81b470aabd65c6dbaf3
makoto_kw <makoto.kw@gmail.com>
2015-04-15 11:28:16 +0900

    Removed search form from 404

--- 404.php
+++ 404.php
@@ -10,22 +10,10 @@ get_header(); ?>
 	<div class="site-content-area ">
 		<main class="site-content site-content-error404" role="main">
 			<div class="container">
-
 				<header class="page-header">
 					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'makotokw' ); ?></h1>
 				</header>
-
 			</div>
-
-			<div class="page-wrapper">
-				<div class="page-content">
-					<div class="container">
-						<?php get_search_form(); ?>
-					</div>
-				</div>
-
-			</div>
-
 		</main>
 	</div>
 
