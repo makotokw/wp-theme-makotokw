7a6dc9aeb84b0296916b617d14fc0818b82de885
makoto_kw <makoto.kw@gmail.com>
2019-12-10 21:22:01 +0900

    errorPage to content.no-results style

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,16 +1,11 @@
 <?php
 /**
- * The template part for displaying a message that posts cannot be found.
- *
- * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
  * @package makotokw
  */
 ?>
-
-<section class="errorPage">
-	<header class="errorPage-header">
-		<h1 class="errorPage-title">
+<section class="no-results not-found">
+	<header class="page-header">
+		<h1 class="page-title">
 			<?php if ( is_search() ) : ?>
 				<?php _e( 'Nothing Found', 'makotokw' ); ?>
 			<?php elseif ( is_404() ) : ?>
@@ -18,17 +13,15 @@
 			<?php endif; ?>
 		</h1>
 	</header>
-	<div class="errorPage-wrapper">
-		<div class="errorPage-content">
-			<?php if ( is_search() ) : ?>
-				<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'makotokw' ); ?></p>
-				<?php get_search_form(); ?>
-			<?php elseif ( is_404() ) : ?>
-				<nav class="errorPage-nav"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><i class="fas fa-home"></i> <?php _e( 'Return Home', 'makotokw' ); ?></a></nav>
-			<?php else : ?>
-				<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'makotokw' ); ?></p>
-				<?php get_search_form(); ?>
-			<?php endif; ?>
-		</div>
+	<div class="page-content">
+		<?php if ( is_search() ) : ?>
+			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'makotokw' ); ?></p>
+			<?php get_search_form(); ?>
+		<?php elseif ( is_404() ) : ?>
+			<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><i class="fas fa-home"></i> <?php _e( 'Return Home', 'makotokw' ); ?></a></p>
+		<?php else : ?>
+			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'makotokw' ); ?></p>
+			<?php get_search_form(); ?>
+		<?php endif; ?>
 	</div>
 </section>
