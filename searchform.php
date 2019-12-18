f02b3eaf8743dd57d3f75576acfb744f3de1d8e1
makoto_kw <makoto.kw@gmail.com>
2019-12-18 22:46:48 +0900

    removed unuse global styles

--- searchform.php
+++ searchform.php
@@ -10,8 +10,5 @@
 	<input type="text" class="search-form-text" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" placeholder="<?php _e( 'Search the site', 'makotokw' ); ?>"/>
 	<button type="submit" class="btn btn-contained btn-search">
 		<i class="fas fa-search"></i>
-		<span class="screen-reader-text">
-			<?php echo _x( 'Search', 'submit button', 'makotokw' ); ?>
-		</span>
 	</button>
 </form>
